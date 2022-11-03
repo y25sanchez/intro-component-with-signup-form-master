28ab0108a49156d48a79f48c41386510c605e0a6
Yefry Sanchez <y25sanchez@gmail.com>
2022-11-03 14:20:27 -0400

    validation error text fixed

--- validation.js
+++ validation.js
@@ -4,39 +4,46 @@ const email = document.getElementById("email");
 const pass = document.getElementById("pass");
 const form = document.getElementById("form");
 
-
 form.addEventListener("submit", (e) => {
   if (first.value === "") {
     e.preventDefault();
+    first.nextSibling.remove();
     first.classList.add("err");
-    let text = document.createElement("p")
-    text.innerText = "First Name cannot be empty"
-    text.classList.add("error")
-    first.parentNode.insertBefore(text, first.nextSibling)
+    let text = document.createElement("p");
+    text.innerText = "First Name cannot be empty";
+    text.classList.add("error");
+    first.parentNode.insertBefore(text, first.nextSibling);
   }
   if (last.value === "") {
     e.preventDefault();
+    last.nextSibling.remove();
     last.classList.add("err");
-    let text = document.createElement("p")
-    text.innerText = "Last Name cannot be empty"
-    text.classList.add("error")
-    last.parentNode.insertBefore(text, last.nextSibling)
+    let text = document.createElement("p");
+    text.innerText = "Last Name cannot be empty";
+    text.classList.add("error");
+    last.parentNode.insertBefore(text, last.nextSibling);
   }
-  if (email.value === "" || !email.value.includes("@") || !email.value.includes(".com")) {
+  if (
+    email.value === "" ||
+    !email.value.includes("@") ||
+    !email.value.includes(".com")
+  ) {
     e.preventDefault();
+    email.nextSibling.remove();
     email.classList.add("err");
-    let text = document.createElement("p")
-    text.innerText = "Looks like this in not an email"
-    text.classList.add("error")
-    email.parentNode.insertBefore(text, email.nextSibling)
+    let text = document.createElement("p");
+    text.innerText = "Looks like this in not an email";
+    text.classList.add("error");
+    email.parentNode.insertBefore(text, email.nextSibling);
   }
   if (pass.value === "") {
     e.preventDefault();
+    pass.nextSibling.remove();
     pass.classList.add("err");
-    let text = document.createElement("p")
-    text.innerText = "Password cannot be empty"
-    text.classList.add("error")
-    pass.parentNode.insertBefore(text, pass.nextSibling)
+    let text = document.createElement("p");
+    text.innerText = "Password cannot be empty";
+    text.classList.add("error");
+    pass.parentNode.insertBefore(text, pass.nextSibling);
   }
 });
 
